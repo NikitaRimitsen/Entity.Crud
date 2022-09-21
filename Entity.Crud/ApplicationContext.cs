c081bc3a71e8e25476a04a47fbc1a91832723e25
opilane <opilane@HP-CZC6297B9B>
2022-09-21 11:30:36 +0300

    Add project files.

---
+++ Entity.Crud/ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity.Crud
+{
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public string connectionString;
+        public ApplicationContext(string connectionString)
+        {
+            this.connectionString = connectionString;   // получаем извне строку подключения
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Linn { get; set; }
+    }
+}
---
+++ Entity.Crud/Program.cs
@@ -0,0 +1,11 @@
+﻿using Entity.Crud;
+
+using (ApplicationContext db = new ApplicationContext("Data Source=helloapp.db"))
+{
+    var users = db.Users.ToList();
+    Console.WriteLine("Пользователи:");
+    foreach (User user in users)
+    {
+        Console.WriteLine($"{user.Id}.Nimi:{user.Name} Aasta:{user.Age} Sugu:{user.Gender} Linn:{user.Linn}");
+    }
+}
